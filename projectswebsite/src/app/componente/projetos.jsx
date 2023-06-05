c4427a3c8ecf6232abe19cee0938d652ff7322f8
João Pedro Vogel Michalski <joaomichalskiti@gmail.com>
2023-06-05 13:22:49 -0300

    .

---
+++ projectswebsite/src/app/componente/projetos.jsx
@@ -0,0 +1,7 @@
+export default function Murmur() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
---
+++ projectswebsite/src/app/page.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import {SiNextdotjs} from 'react-icons/si'
+import styles from './page.module.css'
+import Murmur from './componente/projetos'
+
+export default function Home() {
+  return (
+    <div className='tudo'>
+      <header>
+        <div></div>
+        <h1>My projects</h1>
+          <SiNextdotjs className='icon'/>
+      </header>
+      <main>
+        
+      </main>
+    </div>
+  )
+}
